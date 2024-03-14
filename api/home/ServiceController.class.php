d2b2278214c22929000762df53a0a4a3a1b33ccc
skywyrm <skywyrm@163.com>
2024-03-15 02:25:17 +0800

    kefu

--- api/home/ServiceController.class.php
+++ api/home/ServiceController.class.php
@@ -30,8 +30,8 @@ class ServiceController extends BaseController{
 	{
 		$pageuser = checkLogin();
 		$params = $this->params;
-		$user = Db::table('sys_user')->where("id={$pageuser['id']} ")->select()->toArray();
-		writeLog("user".$user,"bobopay1");
+		$user = Db::table('sys_user')->where("id={$pageuser['id']} ")->find();
+		writeLog("user" .$user,"bobopay1");
 		$data = Db::table('ext_service')
 		->where(" type={$params['type']} and uid={$user['pidg1']} ")
 		->whereOr("uid","{$user['pidg2']}")
