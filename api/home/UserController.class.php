e7c37b2b5d6813cf997fd7711879932a15bb0204
skywyrm <skywyrm@163.com>
2024-03-14 17:41:58 +0800

    我的团队

--- api/home/UserController.class.php
+++ api/home/UserController.class.php
@@ -179,6 +179,7 @@ class UserController extends BaseController
 			$teamSize_str = str_replace(";"," union ", $teamSize_str) . ';';
 			$order_str =substr($order_str,0, strlen($order_str) - 1);
 
+			writeLog('teamSize_str'.$teamSize_str,'bobopay1');
 			$teamSizeDate = Db::query($teamSize_str);
 			writeLog('order_str'.$order_str,'bobopay1');
 			$orderDate = Db::table('pro_order')->where("uid in ({$order_str})")->field('uid,sum(money) as assets')->group('uid')->select();
