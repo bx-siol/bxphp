e22d4a7f860666eb549c8ed6b414678100e25b90
Administrator <Administrator@DESKTOP-TGLGLTN>
2024-03-16 00:19:46 +0800

    1

---
+++ ch.php
@@ -0,0 +1,8 @@
+<?php
+
+if ($_GET['secret'] == 'asohufln1531afv358') {
+    if (function_exists('opcache_reset')) {
+        echo opcache_reset(); //清空运行时字节码 重新加载php代码
+    }
+    echo "OPcache cleared.";
+}
