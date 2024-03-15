5ee3665229f6e91196fa8e3f03559a7aaaa01007
skywyrm <skywyrm@163.com>
2024-03-16 02:29:52 +0800

    团队

--- api/home/UserController.class.php
+++ api/home/UserController.class.php
@@ -174,7 +174,7 @@ class UserController extends BaseController
 			foreach ($list as &$v) {
 				$referrer_str .= $v["id"] . ",";
 				$teamSize_str .= "select {$v['id']} as id,count(1) as teamSize  from sys_user where pids like '%{$v['id']}%';";
-				$order_str .= $v["id"] . ",";
+				$amount_str .= "select {$v['id']} as pid,id from syso_user where pids like '%{&v['id']}%';";
 			}
 
 			$referrerData = array();
@@ -192,12 +192,40 @@ class UserController extends BaseController
 				$teamSizeDate = Db::query($teamSize_str);
 			}				
 			
-			$orderDate = array();
-			if($order_str)
+			$amountDate = array();
+			if($amount_str)
 			{
-				$order_str =substr($order_str,0, strlen($order_str) - 1);
-				$orderDate = Db::table('pro_order')->where("uid in ({$order_str})")->field('uid,sum(money) as assets')->group('uid')->select();
-			}				
+				$amount_str =substr($amount_str,0, strlen($amount_str) - 1);
+				$amount_str = str_replace(";"," union ", $amount_str) . ';';
+				$amountDate = Db::query($teamSize_str);
+			}
+			$dic = array();
+			$amount_ids = "";
+			if(count($referrerData) > 0)
+			{
+				foreach($amountDate as $it)
+				{
+					if (strpos($amount_ids, $it["id"]) == false)
+    					$amount_ids .= $it["id"] . ",";
+
+					if(isset($dic["{$it['pid']}"]))
+					{
+						$val = $dic["{$it['pid']}"];
+						array_push($val,$it['id']);
+						$dic["{$it['pid']}"] = $val;
+					}
+					else
+					{
+						$dic["{$it['pid']}"] = array($it['id']);
+					}
+				}
+				if(empty($amount_ids))
+					$amount_ids =substr($amount_ids,0, strlen($amount_ids) - 1);
+			}
+			writeLog(json_encode($dic),"bobopay1");
+
+			$amountData = Db::table("pro_order")->where("uid in ({$amount_ids})")->field('uid,sum(money) as money')->group('uid')->select();
+
 
 			foreach ($list as &$item) {
 				$item["referrer"] = 0;
@@ -214,8 +242,8 @@ class UserController extends BaseController
 						if($item["id"] == $v['id'])
 							$item["teamSize"] = $v["teamSize"];
 
-				if(count($orderDate) > 0)
-					foreach ($orderDate as &$v)
+				if(count($amountData) > 0)
+					foreach ($amountData as &$v)
 						if ($item["id"] == $v["uid"])
 							$item["assets"] = $v['assets'];
 
@@ -243,7 +271,7 @@ class UserController extends BaseController
 				'lv' => $lv,
 				'$total_page' => $total_page,
 			];
-			$this->redis->set($mem_key, $return_data, 300);
+			$this->redis->set($mem_key, $return_data, 86400);
 		}
 		ReturnToJson(1, 'ok', $return_data);
 	}
