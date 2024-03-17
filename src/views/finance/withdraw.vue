219c786e3625f95fcd30ff899f754ff2441c44af
skywyrm <skywyrm@163.com>
2024-03-17 22:33:04 +0800

    提现范围

--- src/views/finance/withdraw.vue
+++ src/views/finance/withdraw.vue
@@ -245,14 +245,14 @@ const onSubmit = () => {
         }
     }
 
-    if (dataForm.money < ptype.min) {
+    if (dataForm.money < min.value) {
         isRequest = false
-        _alert(ptype.name + ' Minimum withdrawal amount is ' + ptype.min)
+        _alert(' Minimum withdrawal amount is ' + min.value)
         return
     }
-    if (dataForm.money > ptype.max) {
+    if (dataForm.money > max.value) {
         isRequest = false
-        _alert(ptype.name + ' Maximum withdrawal amount is ' + ptype.max)
+        _alert(' Maximum withdrawal amount is ' + max.value)
         return
     }
 
