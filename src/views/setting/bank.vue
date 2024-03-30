2c5af44f53fb0e7b46156418515914e769884341
skywyrm <skywyrm@163.com>
2024-03-30 22:09:56 +0800

    保存银行卡

--- src/views/setting/bank.vue
+++ src/views/setting/bank.vue
@@ -246,7 +246,7 @@ onMounted(() => {
                 dataForm.ifsc = res.data.bank.ifsc
                 for (let i in res.data.bank_arr) {
                     if (res.data.bank_arr[i].code == res.data.bank.bank_id) {
-                        bankIdx.value = i * 1
+                        bankIdx.value = res.data.bank_arr[i].id
                     }
                 }
             }
