c01b85b6212d21a4cea0b5ba9e720020ebace38b
skywyrm <skywyrm@163.com>
2024-04-02 17:24:12 +0800

    11111

--- api/notify/CowpayController.class.php
+++ api/notify/CowpayController.class.php
@@ -22,9 +22,11 @@ class CowpayController extends BaseController
         //$jsonStr = "{\"sign\":\"C79823EBABAC1C7F5DB1120AAA1A24F8\",\"transdata\":\"%7B%22order_no%22%3A%22e08038298e26f1a4%22%2C%22order_time%22%3A1712048747000%2C%22product_code%22%3A%221%22%2C%22product_name%22%3A%22e08038298e26f1a4%22%2C%22order_amount%22%3A%22200.000%22%2C%22pay_type%22%3A%22india-upi-h5%22%2C%22payment%22%3A%22%E6%94%AF%E4%BB%98%E6%88%90%E5%8A%9F%22%7D\"}";
         $params = json_decode($jsonStr, true); 
         writeLog("params：".json_encode($params, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), 'cowpay/notify/pay');
-
+        $rdata = json_decode(urldecode($params["transdata"]), true);
+        
+        writeLog("rdata".json_encode($rdata, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), 'cowpay/notify/pay');
         require_once APP_PATH . 'common/pay/cowpay.php';
-        $sign = paySign(urldecode($params["transdata"]), true);
+        $sign = paySign($rdata, true);
         writeLog($sign,'cowpay/notify/pay');
         if ($sign != $params['sign'])
             ReturnToJson(-1, 'Sign error');
