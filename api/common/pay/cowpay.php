83a2380dd85f19faaee3eb3550a01ed18279ce2b
skywyrm <skywyrm@163.com>
2024-04-02 14:59:29 +0800

    1111

--- api/common/pay/cowpay.php
+++ api/common/pay/cowpay.php
@@ -28,6 +28,7 @@ function payOrder($fin_paylog, $sub_type = '')
 
 	writeLog("pdata：" .json_encode($pdata)."\r\n"."rdata：".json_encode($rdata), GetPayName() . '/pay');
 	$result = curl_post($config['pay_url'], $rdata, 30,'json');
+	writeLog(json_encode($result, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/pay');
 	if ($result['code'] != 200)
 		return $result;
 
