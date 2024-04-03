286c71f1b4eb9071c8b4d9c8e9ffd6e04e5b9452
skywyrm <skywyrm@163.com>
2024-04-04 01:07:13 +0800

    1111

--- src/views/finance/cashlog.vue
+++ src/views/finance/cashlog.vue
@@ -142,12 +142,7 @@
                     </template>
                 </el-table-column>
 
-                <el-table-column prop="pay_type_bf" :label="isTrans ? 'Channel' : '代付通道'" width="100" fixed>
-                    <!-- <template #default="{ row }">
-                        <div v-if="row.pay_type == null || row.pay_type == ''">{{row.pay_type_bf}}</div>
-                        <div v-else>{{row.pay_type}}</div>
-                    </template> -->
-                </el-table-column>
+                <el-table-column prop="pay_type_bf" :label="isTrans ? 'Channel' : '代付通道'" width="100" fixed></el-table-column>
                 <el-table-column prop="pay_status_flag" :label="isTrans ? 'Payment status' : '代付状态'" width="100" fixed>
                     <template #default="{ row }">
                         {{ row.pay_status_flag }}
