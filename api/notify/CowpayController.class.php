b4e48b1bd698c4423081e71cbacef72bea524249
skywyrm <skywyrm@163.com>
2024-04-03 20:12:03 +0800

    1111

--- api/notify/CowpayController.class.php
+++ api/notify/CowpayController.class.php
@@ -39,8 +39,8 @@ class CowpayController extends BaseController
     {
         $jsonStr = trim(file_get_contents('php://input'));
         writeLog('pdatajwt : ' . $jsonStr, 'cowpay/notify/cash');
-        $jsonStr="{\"sign\":\"C15E862E12F182C783A59F4DE2434040\",\"transdata\":\"%7B%22order_no%22%3A%22b7b6ea5b987bbbcf%22%2C%22message%22%3A%22%E6%8F%90%E7%8E%B0%E6%88%90%E5%8A%9F%22%2C%22order_amount%22%3A%22385.000%22%2C%22resp_code%22%3A%22S%22%7D\"}";
-        //$jsonStr="{\"sign\":\"434F3DDC6AF071B83AC4DB92483CCF1D\",\"transdata\":\"%7B%22order_no%22%3A%22a4706629902b5d66%22%2C%22message%22%3A%22%E6%8F%90%E7%8E%B0%E6%88%90%E5%8A%9F%22%2C%22order_amount%22%3A%22208.000%22%2C%22resp_code%22%3A%22S%22%7D\"}";
+        //$jsonStr="{\"sign\":\"C15E862E12F182C783A59F4DE2434040\",\"transdata\":\"%7B%22order_no%22%3A%22b7b6ea5b987bbbcf%22%2C%22message%22%3A%22%E6%8F%90%E7%8E%B0%E6%88%90%E5%8A%9F%22%2C%22order_amount%22%3A%22385.000%22%2C%22resp_code%22%3A%22S%22%7D\"}";
+        $jsonStr="{\"sign\":\"434F3DDC6AF071B83AC4DB92483CCF1D\",\"transdata\":\"%7B%22order_no%22%3A%22a4706629902b5d66%22%2C%22message%22%3A%22%E6%8F%90%E7%8E%B0%E6%88%90%E5%8A%9F%22%2C%22order_amount%22%3A%22208.000%22%2C%22resp_code%22%3A%22S%22%7D\"}";
         $params = json_decode($jsonStr, true);
         $rdata = json_decode(urldecode($params["transdata"]), true);
 
