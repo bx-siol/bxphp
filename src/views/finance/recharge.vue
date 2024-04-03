1a00ae04a01de719da65ffedca31137f949ab6f6
anilybing <30265407+anilybing@users.noreply.github.com>
2024-04-03 16:01:40 +0800

    充值字符限制

--- src/views/finance/recharge.vue
+++ src/views/finance/recharge.vue
@@ -138,6 +138,7 @@ const onclickPayItem = (idx: number, mval: any) => {
 }
 
 const onKyupAmount = () => {
+    money.value = money.value.replace(/\D/g, '');
     itemIdx.value = -1
 }
 const store = useStore()
