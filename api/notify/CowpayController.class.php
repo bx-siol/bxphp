f63f4a9a867fa27eff61fa450ecdb8c8f11875e9
skywyrm <skywyrm@163.com>
2024-04-03 19:51:58 +0800

    11111

--- api/notify/CowpayController.class.php
+++ api/notify/CowpayController.class.php
@@ -43,11 +43,12 @@ class CowpayController extends BaseController
         //$jsonStr="{\"sign\":\"434F3DDC6AF071B83AC4DB92483CCF1D\",\"transdata\":\"%7B%22order_no%22%3A%22a4706629902b5d66%22%2C%22message%22%3A%22%E6%8F%90%E7%8E%B0%E6%88%90%E5%8A%9F%22%2C%22order_amount%22%3A%22208.000%22%2C%22resp_code%22%3A%22S%22%7D\"}";
         $params = json_decode($jsonStr, true);
         writeLog('urldecode : ' . urldecode($params["transdata"]), 'cowpay/notify/cash');
-        $rdata = json_decode(urldecode($params["transdata"]), true);
+        $rdata = json_decode($params["transdata"], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
 
         $rdata["message"] = mb_convert_encoding($rdata["message"],'UTF-8','UTF-16');
 
-        writeLog('rdata : ' . json_encode($rdata), 'cowpay/notify/cash');
+        writeLog('rdata : ' . json_encode($rdata, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), 'cowpay/notify/cash');
+        writeLog('rdata1 : ' . $rdata["message"], 'cowpay/notify/cash');
         require_once APP_PATH . 'common/cash/cowpay.php';
         $sign = CashSign($params);
         writeLog('sign : ' . $sign, 'cowpay/notify/cash');
