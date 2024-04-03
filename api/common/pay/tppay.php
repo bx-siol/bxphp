ddcaf59296489c521a4cf1e4533df7ac75fb7b39
skywyrm <skywyrm@163.com>
2024-04-03 17:10:08 +0800

    1111

--- api/common/pay/tppay.php
+++ api/common/pay/tppay.php
@@ -32,7 +32,7 @@ function payOrder($fin_paylog, $sub_type = '')
 	writeLog(json_encode($pdata, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/pay');
 	$result = [];
 	try {
-		$result = curl_post($config['pay_url'], $pdata, 30);
+		$result = CurlPost($config['pay_url'], $pdata, 30);
 	} catch (\Throwable $th) {
 		return ['code' => -1, 'msg' => ''];
 	}
