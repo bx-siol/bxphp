4d3bbffacd2072d3f10f19c5eaca3b5d79a2d77a
anilybing <30265407+anilybing@users.noreply.github.com>
2024-04-03 16:31:21 +0800

    add tppay payment

---
+++ api/common/cash/tppay.php
@@ -0,0 +1,62 @@
+<?php
+
+use Curl\Curl;
+use think\facade\Db;
+
+
+
+function GetPayName()
+{
+	return "tppay";
+}
+function CashOrder($fin_cashlog)
+{
+	$config = $_ENV['PAY_CONFIG'][GetPayName()];
+	$microtime = microtime(true); // 获取浮点数形式的当前时间戳
+	$milliseconds = round($microtime * 1000); // 将时间戳转换为毫秒级
+	$pdata = [
+		'outType' => 'IMPS',
+		'merchantId' => $config['mch_id'],
+		'orderId' => $fin_cashlog['osn'],
+		'timestamp' => $milliseconds,
+		'amount' => floor($fin_cashlog['real_money'] * 100),
+		'accountHolder' => $fin_cashlog['receive_realname'],
+		'accountNumber' => ($fin_cashlog['receive_account']),
+		'ifsc' => $fin_cashlog['receive_ifsc'],
+		'notifyUrl' => $config['dnotify_url']
+	];
+	$pdata['sign'] = CashSign($pdata);
+	$url = $config['dpay_url'];
+	writeLog(json_encode($pdata, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/cash');
+	$result = CurlPost($url, $pdata, 30);
+	if ($result['code'] != 1)
+		return $result;
+	$resultArr = $result['output'];
+	writeLog(json_encode($resultArr, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/cash');
+	if ($resultArr['code'] != '100') {
+		writeLog('result : ' . json_encode($resultArr, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/cash/error');
+		return ['code' => -1, 'msg' => $resultArr['msg']];
+	}
+
+	$return_data = [
+		'code' => 1,
+		'msg' => $result['msg'],
+		'data' => [
+			'mch_id' => $config['mch_id'],
+			'osn' => $fin_cashlog['osn'],
+			'out_osn' => $resultArr['payOrderId']
+		]
+	];
+	return $return_data;
+}
+
+function CashSign($params)
+{
+	// sign	是	string	签名，md5(amount+merchantId+orderId+timestamp+secret)进行MD5加密，32位小写。
+	$config = $_ENV['PAY_CONFIG'][GetPayName()];
+	$signStr = $params['amount'] . $params['merchantId'] . $params['orderId'] . $params['timestamp'] . $config['mch_key'];
+	//writeLog(json_encode('signStr : ' . $signStr, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName());
+	$outstr = strtolower(md5($signStr));
+	//writeLog(json_encode('outstr : ' . $outstr, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName());
+	return $outstr;
+}
---
+++ api/common/pay/tppay.php
@@ -0,0 +1,243 @@
+<?php
+
+use Curl\Curl;
+
+
+function GetPayName()
+{
+	return "tppay";
+}
+
+
+function payOrder($fin_paylog, $sub_type = '')
+{
+	$config = $_ENV['PAY_CONFIG'][GetPayName()];
+	$microtime = microtime(true); // 获取浮点数形式的当前时间戳
+	$timestamp = round($microtime * 1000); // 将时间戳转换为毫秒级
+	$pdata = [
+
+		// amount	是	integer	订单金额，单位分，例金额为102.33，则填： 10233
+		// merchantId	是	integer	商户号，开户后提供，例： 100
+		// orderId	是	string	商户订单号，50个字符以内，例：12345678
+		// timestamp	是	long	时间戳，精确到毫秒，例：1687510573621
+		// notifyUrl	是	string	回调通知地址，例：http://localhost/tppay
+		// sign	是	string	签名，md5(amount+merchantId+orderId+timestamp+密钥)进行MD5，32位小写加密。密钥开户后提供.
+		'merchantId' => $config['mch_id'],
+		'orderId' => $fin_paylog['osn'],
+		'amount' =>  $fin_paylog['money'] * 100,
+		'timestamp' => $timestamp,
+		'notifyUrl' => $config['notify_url'],
+	];
+	$pdata['sign'] = paySign($pdata);
+	writeLog(json_encode($pdata, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/pay');
+	$result = [];
+	try {
+		$result = CurlPost($config['pay_url'], $pdata, 30);
+	} catch (\Throwable $th) {
+		return ['code' => -1, 'msg' => ''];
+	}
+	//writeLog('result : ' . json_encode($result, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName().'/pay');
+	if ($result['code'] != 1) {
+		return $result;
+	}
+	$resultArr = $result['output'];
+	if ($resultArr['code'] != '100') {
+		writeLog('result : ' . json_encode($resultArr, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/pay/error');
+		return ['code' => -1, 'msg' => 'Channel is not open'];
+	}
+	$return_data = [
+		'code' => 1,
+		'msg' => $result['msg'],
+		'data' => [
+			'mch_id' => $config['mch_id'],
+			'osn' => $fin_paylog['osn'],
+			'out_osn' => $resultArr['payOrderId'],
+			'pay_url' => $resultArr['paymentUrl']
+		]
+	];
+	return $return_data;
+}
+
+//查询订单状态
+function status($orderId)
+{
+	$config = $_ENV['PAY_CONFIG'][GetPayName()];
+	$microtime = microtime(true); // 获取浮点数形式的当前时间戳
+	$timestamp = round($microtime * 1000); // 将时间戳转换为毫秒级
+	$pdata = [
+		'merchantId' => $config['mch_id'],
+		'timestamp' => $timestamp,
+		'orderId' => $orderId,
+	];
+	$pdata['sign'] = paySign($pdata, 1);
+	$url = $config['payquery_url'];
+	//writeLog( json_encode($pdata, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/balance');
+	$result = CurlPost($url, $pdata, 30);
+	if ($result['code'] != 1)
+		return $result;
+	$resultArr = $result['output'];
+	writeLog(json_encode($resultArr, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/status');
+	if ($resultArr['code'] != 100) {
+		writeLog('result : ' . json_encode($resultArr, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/status/error');
+		return ['code' => -1, 'msg' => $resultArr['msg']];
+	}
+	$stat = 0;
+	switch ($resultArr['stat']) {
+		case '1':
+			$stat = 1;
+			break;
+		case '2':
+			$stat = 2;
+			break;
+	}
+	$return_data = [
+		'code' => 1,
+		'msg' => $resultArr['msg'],
+		'data' => [
+			'merId' => $config['mch_id'],
+			'orderId' => $resultArr['orderId'], //匹配的订单号，只有stat结果为2时返回
+			'stat' => $stat, //0未收到UTR，1收到UTR但未匹配订单可以补单，2已匹配订单）
+			'amount' => $resultArr['amount'] / 100, //入账金额，单位分，只有stat结果为1时返回，示例：100.00时返回10000 
+		]
+	];
+	return $return_data;
+}
+
+//utr 补单
+function utrorder($fin_paylog)
+{
+	$config = $_ENV['PAY_CONFIG'][GetPayName()];
+	$microtime = microtime(true); // 获取浮点数形式的当前时间戳
+	$timestamp = round($microtime * 1000); // 将时间戳转换为毫秒级
+	$pdata = [
+		'merchantId' => $config['mch_id'],
+		'timestamp' => $timestamp,
+		'utr' => $fin_paylog['utr'],
+		'orderId' => $fin_paylog['osn'],
+	];
+	$pdata['sign'] = paySign($pdata, 1);
+	$url = $config['utrorder_url'];
+	//writeLog( json_encode($pdata, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/balance');
+	$result = CurlPost($url, $pdata, 30);
+	if ($result['code'] != 1)
+		return $result;
+	$resultArr = $result['output'];
+	writeLog(json_encode($resultArr, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/utrorder');
+	if ($resultArr['code'] != 100) {
+		writeLog('result : ' . json_encode($resultArr, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/utrorder/error');
+		return ['code' => -1, 'msg' => $resultArr['msg']];
+	}
+	$return_data = [
+		'code' => 1,
+		'msg' => $resultArr['msg'],
+		'data' => [
+			'merId' => $config['mch_id'],
+			'orderId' => $fin_paylog['osn'],
+			'utr' => $fin_paylog['utr'],
+		]
+	];
+	return $return_data;
+}
+
+//utr 查单
+function utr($utr)
+{
+	$config = $_ENV['PAY_CONFIG'][GetPayName()];
+	$microtime = microtime(true); // 获取浮点数形式的当前时间戳
+	$timestamp = round($microtime * 1000); // 将时间戳转换为毫秒级
+	$pdata = [
+		'merchantId' => $config['mch_id'],
+		'timestamp' => $timestamp,
+		'utr' => $utr,
+	];
+	$pdata['sign'] = paySign($pdata, 1);
+	$url = $config['utrorder_url'];
+	//writeLog( json_encode($pdata, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/balance');
+	$result = CurlPost($url, $pdata, 30);
+	if ($result['code'] != 1)
+		return $result;
+	$resultArr = $result['output'];
+	writeLog(json_encode($resultArr, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/utr');
+	if ($resultArr['code'] != 100) {
+		writeLog('result : ' . json_encode($resultArr, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/utr/error');
+		return ['code' => -1, 'msg' => $resultArr['msg']];
+	}
+	$stat = 0;
+	switch ($resultArr['stat']) {
+		case '1':
+			$stat = 1;
+			break;
+		case '2':
+			$stat = 2;
+			break;
+	}
+	$return_data = [
+		'code' => 1,
+		'msg' => $resultArr['msg'],
+		'data' => [
+			'merId' => $config['mch_id'],
+			'orderId' => $resultArr['orderId'], //匹配的订单号，只有stat结果为2时返回
+			'stat' => $stat, //0未收到UTR，1收到UTR但未匹配订单可以补单，2已匹配订单）
+			'amount' => $resultArr['amount'] / 100, //入账金额，单位分，只有stat结果为1时返回，示例：100.00时返回10000 
+		]
+	];
+	return $return_data;
+}
+
+//查询余额
+function balance()
+{
+	$config = $_ENV['PAY_CONFIG'][GetPayName()];
+	$microtime = microtime(true); // 获取浮点数形式的当前时间戳
+	$timestamp = round($microtime * 1000); // 将时间戳转换为毫秒级
+	$pdata = [
+		'merchantId' => $config['mch_id'],
+		'timestamp' => $timestamp,
+	];
+	$pdata['sign'] = paySign($pdata, 1);
+	$url = $config['balance_url'];
+	//writeLog( json_encode($pdata, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/balance');
+	$result = CurlPost($url, $pdata, 30);
+	if ($result['code'] != 1)
+		return $result;
+	$resultArr = $result['output'];
+	writeLog(json_encode($resultArr, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/balance');
+	if ($resultArr['code'] != 100) {
+		writeLog('result : ' . json_encode($resultArr, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/balance/error');
+		return ['code' => -1, 'msg' => $resultArr['msg']];
+	}
+	$return_data = [
+		'code' => 1,
+		'msg' => $resultArr['msg'],
+		'data' => [
+			'merId' => $config['mch_id'],
+			'balance' => $resultArr['balance'] / 100,
+			'payout_balance' => $resultArr['balance'] / 100,
+		]
+	];
+	return $return_data;
+}
+
+
+
+function paySign($params, $type = 0)
+{
+	$config = $_ENV['PAY_CONFIG'][GetPayName()];
+	$signStr = '';
+	switch ($type) {
+		case 0: //代收
+			// md5(amount+merchantId+orderId+timestamp+密钥)进行MD5，32位小写加密。密钥开户后提供.
+			//(amount+merchantId+orderId+timestamp+密钥)
+			$signStr = $params['amount'] . $params['merchantId'] . $params['orderId'] . $params['timestamp'] . $config['mch_key'];
+			break;
+		case 1: ////查询余额 查询urt 补单utr
+			//签名，md5(merchantId +timestamp+密钥)进行MD5加密，32位小写。
+			$signStr = $params['merchantId'] . $params['timestamp'] . $config['mch_key'];
+			break;
+	}
+
+	if ($signStr == '')
+		return '';
+	$outstr = strtolower(md5($signStr));
+	return $outstr;
+}
