e60ccb945aeb5119dbab2fb77caf14c0558fee8c
skywyrm <skywyrm@163.com>
2024-04-06 22:43:57 +0800

    1111

--- api/common/pay/kirinpay.php
+++ api/common/pay/kirinpay.php
@@ -25,11 +25,12 @@ function payOrder($fin_paylog, $sub_type = '')
 	writeLog(json_encode($pdata, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/pay');
 	$result = [];
 	try {
-		$result = CurlPost($config['pay_url'], $pdata, 30);
+		$result = curl_post2($config['pay_url'], $pdata, 30);
 	} catch (\Throwable $th) {
 		return ['code' => -1, 'msg' => ''];
 	}
 
+    writeLog(json_encode($result, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/pay/error');
 	if ($result['code'] != 1) {
 		return $result;
 	}
