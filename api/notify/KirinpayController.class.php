3d5b4e28878f4b7a6dcb4a31aad533c96574186e
skywyrm <skywyrm@163.com>
2024-04-06 23:54:14 +0800

    1111

--- api/notify/KirinpayController.class.php
+++ api/notify/KirinpayController.class.php
@@ -17,7 +17,6 @@ class KirinpayController extends BaseController
     public function _pay()
     {
         $jsonStr = trim(file_get_contents('php://input'));
-        $jsonStr = "ext_data=123456789012&callbacks=CODE_SUCCESS&appid=stage&pay_type=upi&pay_time=1712418528&out_trade_no=36dbe47d2cb7bcd4&amount=600.00&amount_true=600.00&out_uid=&sign=999B857DA7110F85A152FBA35F1B6B7A";
         writeLog('jsonStr : ' . $jsonStr, 'kirinpay/notify/pay');
         $params = explode("&", $jsonStr);
         if (!$params)
@@ -34,15 +33,12 @@ class KirinpayController extends BaseController
         if ($sign != $rdata['sign'])
             ReturnToJson(-1, 'Sign error');
 
-        writeLog('rdata : ' . json_encode($rdata), 'kirinpay/notify/pay');
-
         $pdata = [
             'code' => $rdata['callbacks'] == 'CODE_SUCCESS' ? 1 : -1,
             'osn' => $rdata['out_trade_no'],
             'amount' => $rdata['amount'],
             'successStr' => 'success'
         ];
-        writeLog('pdata : ' . json_encode($pdata), 'kirinpay/notify/pay');
         $this->payAct($pdata,'kirinpay');
     }
 
@@ -50,20 +46,27 @@ class KirinpayController extends BaseController
     {
         $jsonStr = trim(file_get_contents('php://input'));
         writeLog('pdatajwt : ' . $jsonStr, 'kirinpay/notify/cash');
-        $params = json_decode($jsonStr, true);
+        $params = explode("&", $jsonStr);
+        if (!$params)
+            $params = $_POST;
+
+        foreach ($params as $k => $v) {
+            $arr = explode("=", $v);
+            $rdata[$arr[0]] = urldecode($arr[1]);
+        }
 
         require_once APP_PATH . 'common/cash/kirinpay.php';
-        $sign = CashSign($params);
+        $sign = CashSign($rdata);
         writeLog('sign : ' . $sign, 'kirinpay/notify/cash');
-        if ($sign != $params['sign'])
+        if ($sign != $rdata['sign'])
             ReturnToJson(-1, 'Sign error');
 
         $pdata = [
-            'osn' => $params['orderId'],
-            'out_osn' => $params['platOrderId'],
-            'pay_status' => $params['status'] == 1 ? 9 : 3,
+            'osn' => $rdata['orderId'],
+            'out_osn' => $rdata['platOrderId'],
+            'pay_status' => $rdata['status'] == 1 ? 9 : 3,
             'pay_msg' => 'success',
-            'amount' => $params['amount'],
+            'amount' => $rdata['amount'],
             'successStr' => 'success',
             'failStr' => 'success'
         ];
