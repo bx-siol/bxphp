a2178c9c89ab510e388e638f8058a3684f1701f7
skywyrm <skywyrm@163.com>
2024-04-06 21:10:16 +0800

    1111

--- api/common/pay/kirinpay.php
+++ api/common/pay/kirinpay.php
@@ -71,6 +71,7 @@ function balance()
 		writeLog(json_encode($resultArr, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/balance/error');
 		return ['code' => -1, 'msg' => $resultArr['msg']];
 	}
+    writeLog(json_encode($resultArr, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/balance');
     writeLog("11111" , GetPayName() . '/balance');
     writeLog("freeze_balance：" .$resultArr['data']['freeze_balance'] , GetPayName() . '/balance');
     writeLog("22222" , GetPayName() . '/balance');
