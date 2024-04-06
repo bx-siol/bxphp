4d0afd56e4e907002109be635733d545e4b524ad
skywyrm <skywyrm@163.com>
2024-04-06 22:09:14 +0800

    1111

--- api/common/pay/kirinpay.php
+++ api/common/pay/kirinpay.php
@@ -74,7 +74,7 @@ function balance()
     $data = json_decode($resultArr['data'], true);
     writeLog(json_encode($resultArr, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/balance');
     writeLog("11111" , GetPayName() . '/balance');
-    writeLog("data" .json_encode($data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) , GetPayName() . '/balance');
+    writeLog("data" .$resultArr['data'][0] , GetPayName() . '/balance');
     writeLog("22222" , GetPayName() . '/balance');
     $payout_balance = floatval($resultArr['data']['freeze_balance']) +floatval($resultArr['data']['total_money'])+floatval($resultArr['data']['use_balance']);
 	$return_data = [
