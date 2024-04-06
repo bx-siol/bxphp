bbe15df171e1da3dee779a3b9ba379653ae5b06b
skywyrm <skywyrm@163.com>
2024-04-06 22:26:16 +0800

    kirinpay

--- api/notify/KirinpayController.class.php
+++ api/notify/KirinpayController.class.php
@@ -64,7 +64,7 @@ class KirinpayController extends BaseController
     {
 		$params = $this->params;
         $fin_cashlog = Db::table('fin_cashlog')->where("id={$params['id']}")->find();
-        require_once APP_PATH . 'common/cash/xdpay.php';
+        require_once APP_PATH . 'common/cash/kirinpay.php';
         $result = CashOrder($fin_cashlog);
         
 	    return $result;
