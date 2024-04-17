32592bb41a0b63e7cf469395fbc9e07632c4fe25
anilybing <30265407+anilybing@users.noreply.github.com>
2024-04-17 16:58:52 +0800

    11

--- src/components/Purchase.vue
+++ src/components/Purchase.vue
@@ -114,10 +114,6 @@
                                     ₹{{ cutOutNum(item.rate * item.price * item.total_days * item.num / 100, 2) }}
                                   </span>
                                 </div>
-                                <div style="position: absolute;top: 6.4rem;right: 1rem;">
-                                  <span></span>
-                                  <span>{{ item.create_time.substring(0,5) }}</span>
-                                </div>
                                 <div class="dailyearnings" v-if="false">
                                   <span>{{ t('收到的天数') }}: </span>
                                   <span style="color: #64503e;">{{ item.total_days }}</span>
@@ -189,10 +185,6 @@
                                   <span>
                                     ₹{{ cutOutNum(item.rate * item.price * item.total_days * item.num / 100, 2) }}
                                   </span>
-                                </div>
-                                <div style="position: absolute;top: 6.4rem;right: 1rem;">
-                                  <span></span>
-                                  <span>{{ item.create_time.substring(0,5) }}</span>
                                 </div>
                                 <div class="dailyearnings" v-if="false">
                                   <span>{{ t('收到的天数') }}: </span>
