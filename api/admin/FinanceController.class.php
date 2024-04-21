a96497840b8423409ce423b2dc97485e88b6fedf
skywyrm <skywyrm@163.com>
2024-04-21 14:32:48 +0800

    提现加日志

--- api/admin/FinanceController.class.php
+++ api/admin/FinanceController.class.php
@@ -1093,6 +1093,7 @@ class FinanceController extends BaseController
 		} else {
 			foreach ($ids as $item_id) {
 				$result = $this->cashlogCheckAct($pageuser, $item_id, $params['status'], $params['s_paytype'], '', $params['s_paytype']);
+				writeLog(json_encode($result),'111111');
 				if ($result['code'] == 1) {
 					Db::commit();
 					$list[] = [
