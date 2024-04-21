91fe9dd08db787a7641388febac2e1dd46389d45
skywyrm <skywyrm@163.com>
2024-04-21 16:54:23 +0800

    1111

--- api/admin/FinanceController.class.php
+++ api/admin/FinanceController.class.php
@@ -1100,7 +1100,7 @@ class FinanceController extends BaseController
 					'status_flag' => $cnf_cashlog_status[$fin_cashlog['status']],
 					'check_remark' => $fin_cashlog['check_remark'],
 					'check_time' => date('d/m/Y H:i', $fin_cashlog['check_time']),
-					'pay_time' => date('d/m/Y H:i', $fin_cashlog['pay_time']),
+					'pay_time' => '/',
 				];
 				if (isset ($v['pay_status'])) {
 					$cnf_cashlog_pay_status = getConfig('cnf_cashlog_pay_status');
