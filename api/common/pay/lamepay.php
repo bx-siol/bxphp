b081143db3b7a9338633e371691fbc6c2e91501f
skywyrm <skywyrm@163.com>
2024-04-22 14:20:57 +0800

    1111111

--- api/common/pay/lamepay.php
+++ api/common/pay/lamepay.php
@@ -64,7 +64,7 @@ function balance()
 	];
 	$pdata['sign'] = paySign($pdata);
 	writeLog(json_encode($pdata, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/balance');
-	$result = curl_post2($config['url'] . $config['balance_url'], $pdata);
+	$result = CurlPost($config['url'] . $config['balance_url'], $pdata);
 
 	if ($result['code'] != 1)
 		return $result;
