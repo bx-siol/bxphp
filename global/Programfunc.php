71d5efd0214ecc7e9a440921861ac61329551c2c
skywyrm <skywyrm@163.com>
2024-04-28 15:49:44 +0800

    111

--- global/Programfunc.php
+++ global/Programfunc.php
@@ -148,7 +148,7 @@ function updateUserTeamCount($uid)
 	inner join  (select {$uid} as id, COUNT(1) as counts from sys_user  where  pids like '%{$uid}%') as b
 	on sys_user.id=b.id
 	SET teamcount = b.counts
-	WHERE sys_user.id ={$uid} and sys_user.b >0";
+	WHERE sys_user.id ={$uid} and b.first_pay_day >0";
 	$down_arr = Db::execute($sql);
 	return $down_arr;
 }
