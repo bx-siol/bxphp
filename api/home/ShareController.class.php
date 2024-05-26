38caa0798264f16e2a354e25afd029b9d8d57ab4
skywyrm <skywyrm@163.com>
2024-05-26 21:47:34 +0800

    111

--- api/home/ShareController.class.php
+++ api/home/ShareController.class.php
@@ -199,12 +199,10 @@ class ShareController extends BaseController
 		$params = $this->params;
 		//and reg_time>1716748200
 		//下级购买
-		$SubordinateBuy = Db::query("select gid,count(*) totalnum from pro_order where  uid in 
-		(select id from sys_user where pid={$params['id']} and first_pay_day >0 )  group by gid;")->toArray();
+		$SubordinateBuy = Db::query("select gid,count(*) totalnum from pro_order where  uid in (select id from sys_user where pid={$params['id']} and first_pay_day >0 )  group by gid");
 
 		//自己领取
-		$MyReceive = Db::query("select gid,count(*) totalnum from pro_order where uid={$pageuser['id']} 
-		and gid in (225,226,227,228) group by gid;")->toArray();
+		$MyReceive = Db::query("select gid,count(*) totalnum from pro_order where uid={$pageuser['id']} and gid in (225,226,227,228) group by gid");
 
 
 		$return_data = [
