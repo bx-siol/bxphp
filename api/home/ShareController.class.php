c1e8d49c9ad6c169075a1243731cf8467ff2b5f2
skywyrm <skywyrm@163.com>
2024-05-27 14:25:57 +0800

    1111

--- api/home/ShareController.class.php
+++ api/home/ShareController.class.php
@@ -217,9 +217,13 @@ class ShareController extends BaseController
 		// $SubordinateBuy = Db::query("select gid,count(*) totalnum from pro_order where  uid in 
 		// (select id from sys_user where pid={$pageuser['id']} and first_pay_day >0 ) and gid ={$gid}   group by gid");
 
+		//当日日期
+		$currentTimestamp = time();
+		$oneMinuteLater = $currentTimestamp - 9000;
+		$oneMinuteLaterDate = date("Y-m-d", $oneMinuteLater);
 		$SubordinateBuy = Db::query("select gid,count(*) totalnum from ( 
 			SELECT uid,gid FROM pro_order where uid in 
-			(select id from sys_user where pid={$pageuser['id']} and first_pay_day > 0 and reg_time>1716748200 ) GROUP BY uid ORDER BY create_time
+			(select id from sys_user where pid={$pageuser['id']} and first_pay_day ={$oneMinuteLaterDate} ) GROUP BY uid ORDER BY create_time
 			) a where gid ={$gid}  group by gid");
 
 		if(is_null($SubordinateBuy[0]['totalnum']))
