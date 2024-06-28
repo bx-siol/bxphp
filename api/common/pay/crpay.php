361694dd03b031b94affb87773b56eaddabc370a
skywyrm <skywyrm@163.com>
2024-06-28 23:13:59 +0800

    1111111

--- api/common/pay/crpay.php
+++ api/common/pay/crpay.php
@@ -38,7 +38,8 @@ function payOrder($fin_paylog, $sub_type = '')
 	if ($result['code'] != 1) {
 		return $result;
 	}
-	$resultArr = $result["output"][0];
+	$resultArr = $result["output"][0];    
+	writeLog(json_encode($resultArr, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/pay');
 	if ($resultArr["code"] != 0) {
 		writeLog(json_encode($resultArr, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/pay/error');
 		return ['code' => -1, 'msg' => 'Channel is not open'];
