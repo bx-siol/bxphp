85b0b2c891af86563b3a737c56d5177bf35aabcd
skywyrm <skywyrm@163.com>
2024-06-28 23:42:37 +0800

    11111111111

--- api/common/pay/crpay.php
+++ api/common/pay/crpay.php
@@ -34,7 +34,6 @@ function payOrder($fin_paylog, $sub_type = '')
 		return ['code' => -1, 'msg' => ''];
 	}
 
-    writeLog(json_encode($result, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), GetPayName() . '/pay');
 	if ($result['code'] != 1) {
 		return $result;
 	}
