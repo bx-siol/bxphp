e509b487f89f775ef1118cab6b2a9d4230c36b13
skywyrm <skywyrm@163.com>
2024-07-28 13:21:16 +0800

    1111111111

--- src/views/default/index.vue
+++ src/views/default/index.vue
@@ -529,7 +529,7 @@ const onReceiveNo = (item: any) => {
 const confirmTip = () => {
   add_cookie("closeIndexTip", "1")
   tipShow.value = false
-  router.push({ path: '/Project' })
+  //router.push({ path: '/Project' })
 }
 
 const add_cookie = (name: any, val: any) => {
