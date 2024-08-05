adc4f0c45fd5c5d7e47851f02c536f9a8e251ad1
skywyrm <skywyrm@163.com>
2024-08-05 20:13:23 +0800

    分享二维码

--- src/views/share/index.vue
+++ src/views/share/index.vue
@@ -4,7 +4,7 @@
         <div style="text-align: center;" class="invite_wrap">
             <div class="qrcode">
                 <!-- <van-image :src="imgFlag(tdata.qrcode)" @click="onPreview(tdata.qrcode)"></van-image> -->                
-                <vue-qrcode :value="montage.urls" :size="10"></vue-qrcode>
+                <vue-qrcode :value="montage.urls"></vue-qrcode>
             </div>
             <div class="idbox">
                 <p style="text-transform: capitalize !important;">{{ t('您的邀请码') }}</p>
