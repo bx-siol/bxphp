d0fbf4c7358f6d47a4732b2c9474f7afe56c2913
skywyrm <skywyrm@163.com>
2024-08-26 14:51:31 +0800

    11111111

--- api/home/ProductController.class.php
+++ api/home/ProductController.class.php
@@ -804,6 +804,12 @@ class ProductController extends BaseController
 			if ($item['pointshop'] == 1) {
 				$this->invest_date_gift($pageuser, $item, $quantity, $pro_order);
 			} else {
+
+				// $check_num = Db::table('pro_order log')
+				// 			->leftJoin('pro_goods c', 'log.gid=c.id')
+				// 			->where("log.uid={$pageuser['id']} and ( log.is_give=0 or c.is_normal = 1 ")
+				// 			->count('id');
+
 				$check_num = Db::table('pro_order')->where("uid={$pageuser['id']} and is_give=0")->count('id');
 				if ($check_num == 0)
 					$pro_order["p3"] = 1;
@@ -977,12 +983,13 @@ class ProductController extends BaseController
 		if (!$wallet1 || !$wallet2) {
 			throw new \Exception('Wallet acquisition exception.');
 		}
-
+		writeLog('开始','aaaaa');
 		//只允许使用充值钱包
 		if (intval($item['buyday']) >= 1) {
 			$w1_money = $discount_total;
 			$w2_money = 0;
 			if (floatval($wallet1['balance']) < $discount_total) {
+				writeLog('555555','aaaaa');
 				ReturnToJson(-1, 'Your balance is insufficient.');
 			}
 		} else {
@@ -992,9 +999,11 @@ class ProductController extends BaseController
 					if ($wallet1['balance'] >= $discount_total) {
 						$w1_money = $discount_total;
 					} else {
+						writeLog('11111','aaaaa');
 						ReturnToJson(-1, 'Your balance is insufficient.');
 					}
 				} else {
+					writeLog('2222','aaaaa');
 					ReturnToJson(-1, 'Your balance is insufficient.');
 				}
 			} else {
@@ -1018,9 +1027,11 @@ class ProductController extends BaseController
 		$pro_order['w2_money'] = $w2_money;
 		Db::table('pro_order')->insertGetId($pro_order);
 		if ($wallet1['balance'] < $w1_money) {
+			writeLog('33333','aaaaa');
 			ReturnToJson(-1, 'Your balance is insufficient.');
 		}
 		if ($wallet2['balance'] < $w2_money) {
+			writeLog('444444','aaaaa');
 			ReturnToJson(-1, 'Your balance is insufficient.');
 		}
 
