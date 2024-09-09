878f88b9390fac5f91ced9407448355163aeae76
skywyrm <skywyrm@163.com>
2024-09-10 02:30:27 +0800

    雀巢添加提款限制

--- api/home/FinanceController.class.php
+++ api/home/FinanceController.class.php
@@ -246,6 +246,9 @@ class FinanceController extends BaseController
 			->find();
 
 			$fin_cashlogSum = Db::table('fin_cashlog')->where(" uid={$pageuser['id']} and create_day = ". TIME_YMD ." ")->sum('money');
+			if($fin_cashlogSum)
+				$fin_cashlogSum = 0;
+			
 			$Withdrawal = 0;
 			switch ($pro_orderMaIid) {
 				case '372':
