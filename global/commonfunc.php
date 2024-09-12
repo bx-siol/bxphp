c19a2f56f61a518d225fb5c1719966b880a92202
skywyrm <skywyrm@163.com>
2024-09-12 22:37:08 +0800

    复盘增加收购送上五级

--- global/commonfunc.php
+++ global/commonfunc.php
@@ -171,7 +171,9 @@ function walletLog($pdata = [], $create_id = 0)
 	];
 	try {
 		$res = Db::table('wallet_log')->insertGetId($db_item);
+		writeLog('2222','asdasdasd');
 	} catch (\Exception $e) {
+		writeLog('33333','asdasdasd');
 		return false;
 	}
 	$db_item['id'] = $res;
