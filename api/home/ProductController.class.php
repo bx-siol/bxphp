2f1076d5ad7311368da0338d672602da665445c9
skywyrm <skywyrm@163.com>
2024-09-14 17:37:44 +0800

    先正达购买指定产品签名

--- api/home/ProductController.class.php
+++ api/home/ProductController.class.php
@@ -1948,7 +1948,7 @@ class ProductController extends BaseController
 		$pro_order = DB::table('pro_order a')
 					->join('pro_goods b',"a.gid = b.id")
 					->where(" a.uid={$pageuser['id']} and b.gsn = '{$params['gsn']}' ")
-					->find();		
+					->find('a.*');		
 		
 		writeLog(json_encode($pro_order),'adasdasd');
 		$data = ['sign' => $params['sign']];
